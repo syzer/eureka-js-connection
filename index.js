2dbc285e2b543f3ed232df20f71bc912bf818725
syzer3@gmail.com <syzer3@gmail.com>
2020-02-04 11:20:09 +0100

    Parser

---
+++ index.js
@@ -0,0 +1,21 @@
+// http://localhost:8761/
+const axios = require('axios')
+const { prop } = require('ramda')
+const { parse } = require('node-html-parser')
+
+const defaultPort = process.env.EUREKA_PORT || 8761
+
+const getAllServices = (port = defaultPort) =>
+  axios.get(`http://localhost:${port}/`)
+    .then(prop('data'))
+    .then(html =>
+      parse(html)
+        .querySelectorAll('a')
+        .filter(e =>
+          e.rawAttrs.includes('actuator')) // ?
+        .map(e => ({
+          url: e.rawAttrs.split('//').pop().split('/').shift(),
+          name: e.innerHTML
+        })))
+
+module.exports = getAllServices
